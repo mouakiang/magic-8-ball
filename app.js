183e92a34b7e0d0abc766b76a63d3cee5e497a33
mouakiang <mouakkia@gmail.com>
2023-01-06 19:00:57 -0500

    added changes to html, added background photo to assets and fixed some errors I kept having

--- app.js
+++ app.js
@@ -1,30 +1,58 @@
 /* Imports */
 
 /* Get DOM Elements */
-const submitButton = document.getElementById('button');
+const submitBtn = document.getElementById('submit');
 const promptSection = document.getElementById('prompt');
-const responseSection = document.getElementById('response');
-const answerSection = document.getElementById('answer');
-const resetButton = document.getAnimations('reset');
+const fortuneSection = document.getElementById('fortune');
+const answerP = document.getElementById('answer');
+const resetBtn = document.getElementById('reset');
 /* State */
 
 /* Events */
-submitButton.addEventListener('click', () => {
+submitBtn.addEventListener('click', () => {
+    //-hide prompt (to hide comment press ctrl forward slash)
+    //-display the image
     toggleSections();
+    //-pick a random number between 0 and length of the answers array – 1
     const randomInt = Math.floor(Math.random() * answers.length);
+
     const randomChoice = answers[randomInt];
+    //-set the content of the answer p to the random choice
+    answerP.textContent = randomChoice;
 });
 
-answerP.textContent = randomChoice;
-
-resetButton.addEventListener('click', () => {
-    toggleSections();
+resetBtn.addEventListener('click', () => {
+    // hide 8 ball and the answer
+    // show prompt
 });
 
 function toggleSections() {
     promptSection.classList.toggle('hide');
-    answerSection.classList.toggle('hide');
+    fortuneSection.classList.toggle('hide');
 }
+
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
